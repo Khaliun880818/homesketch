784091c2add90962547161d2eecd5723ef58cc2f
Khaliun880818 <61443274+Khaliun880818@users.noreply.github.com>
2020-02-27 12:24:25 +0800

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,82 @@
+
+function setup() {
+	createCanvas(windowWidth, windowHeight)
+	background(100)
+	// The big gray rect
+	fill(209)
+	rect(500, 30, 350, 250)
+  fill('white')
+	//The window frame
+	rect(518, 47, 315, 215)
+	fill('rgb(240,248,255)')
+	line(833, 262, 850, 280)
+	line(500, 30, 518, 47)
+	line(850, 30, 833, 47)
+	// Top window
+	rect(533, 58, 285, 40)
+	// Left window
+	rect(533, 108, 100, 143)
+	// Right window
+	rect(650, 108, 167, 143)
+	fill('rgb(154,205,50)')
+	// Green building
+	rect(533, 150, 70, 100)
+	fill('rgb(0,100,0)')
+  rect(533, 150, 20, 100)
+	rect(583, 150, 20, 100)
+	fill('rgb(25,25,112)')
+	triangle(568, 170, 555, 190, 580, 190)
+	fill('rgb(70,130,180)')
+	// Blue building
+	rect(670, 135, 120, 116)
+	rect(700, 130, 56, 5)
+	fill('white')
+	circle(783, 320, 25)
+	line(690, 135, 690, 250)
+	line(710, 135, 710, 250)
+	line(730, 135, 730, 250)
+	line(750, 135, 750, 250)
+	line(770, 135, 770, 250)
+	line(670, 165, 790, 165)
+	line(670, 195, 790, 195)
+	line(670, 225, 790, 225)
+	let w = 10
+	let l = 20
+	// Little windows
+	rect(695, 140, w, l)
+	rect(695, 170, w, l)
+	rect(695, 200, w, l)
+	rect(695, 230, w, l)
+	rect(755, 140, w, l)
+	rect(755, 170, w, l)
+	rect(755, 200, w, l)
+	rect(755, 230, w, l)
+	rect(587, 165, w, l)
+	rect(587, 195, w, l)
+	rect(587, 225, w, l)
+	line(533, 160, 633, 160)
+	line(533, 190, 633, 190)
+	line(533, 220, 633, 220)
+	line(533, 130, 633, 130)
+	fill('rgb(222,184,135)')
+	rect(500, 260, 180, 100)
+	fill('rgb(255,255,240)')
+	rect(680, 300, 100, 60)
+	rect(680, 300, 100, 20)
+	let y = 300
+	let y2 = 320
+	line(680, y, 682, y2)
+	line(690, y, 692, y2)
+	line(700, y, 702, y2)
+	line(710, y, 712, y2)		
+	line(720, y, 722, y2)
+	line(730, y, 732, y2)
+	line(740, y, 742, y2)		
+	line(750, y, 752, y2)
+	line(760, y, 762, y2)
+	line(770, y, 772, y2)
+	line(780, y, 780, 318)
+ 
+
+	
+}
