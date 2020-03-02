6ffb401c9c52a2831214c529a1bba302d37b814a
Khaliun880818 <61443274+Khaliun880818@users.noreply.github.com>
2020-03-02 22:24:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,32 @@
 
-function setup() {
+function setup(one) {
 	createCanvas(windowWidth, windowHeight)
 	background(100)
+	
 	// The big gray rect
 	fill(209)
 	rect(500, 30, 350, 250)
-  fill('white')
-	//The window frame
-	rect(518, 47, 315, 215)
-	fill('rgb(240,248,255)')
+	fill('white')
+     //The window frame
+    rect(518, 47, 315, 215)
+
+	
 	line(833, 262, 850, 280)
 	line(500, 30, 518, 47)
 	line(850, 30, 833, 47)
+
+	fill('rgb(240,248,255)')
 	// Top window
-	rect(533, 58, 285, 40)
-	// Left window
-	rect(533, 108, 100, 143)
-	// Right window
+    rect(533, 58, 285, 40)
+    // Left window
+    rect(533, 108, 100, 143)
+    // Right window
 	rect(650, 108, 167, 143)
-	fill('rgb(154,205,50)')
 	// Green building
+	fill('rgb(154,205,50)')
 	rect(533, 150, 70, 100)
 	fill('rgb(0,100,0)')
-  rect(533, 150, 20, 100)
+    rect(533, 150, 20, 100)
 	rect(583, 150, 20, 100)
 	fill('rgb(25,25,112)')
 	triangle(568, 170, 555, 190, 580, 190)
@@ -32,6 +36,7 @@ function setup() {
 	rect(700, 130, 56, 5)
 	fill('white')
 	circle(783, 320, 25)
+	//window lines
 	line(690, 135, 690, 250)
 	line(710, 135, 710, 250)
 	line(730, 135, 730, 250)
@@ -40,27 +45,17 @@ function setup() {
 	line(670, 165, 790, 165)
 	line(670, 195, 790, 195)
 	line(670, 225, 790, 225)
-	let w = 10
-	let l = 20
+	//let w = 10
+	//let l = 20
 	// Little windows
-	rect(695, 140, w, l)
-	rect(695, 170, w, l)
-	rect(695, 200, w, l)
-	rect(695, 230, w, l)
-	rect(755, 140, w, l)
-	rect(755, 170, w, l)
-	rect(755, 200, w, l)
-	rect(755, 230, w, l)
-	rect(587, 165, w, l)
-	rect(587, 195, w, l)
-	rect(587, 225, w, l)
+	
 	line(533, 160, 633, 160)
 	line(533, 190, 633, 190)
 	line(533, 220, 633, 220)
 	line(533, 130, 633, 130)
 	fill('rgb(222,184,135)')
 	rect(500, 260, 180, 100)
-	fill('rgb(255,255,240)')
+	fill('white')
 	rect(680, 300, 100, 60)
 	rect(680, 300, 100, 20)
 	let y = 300
@@ -76,7 +71,34 @@ function setup() {
 	line(760, y, 762, y2)
 	line(770, y, 772, y2)
 	line(780, y, 780, 318)
- 
-
 	
+	
+
+ 
 }
+function draw(){
+	let w = 10
+	let l = 20
+	let windows = 1*sin(mouseY)
+	let windowss = 1*cos(mouseX)
+	
+	// little windows
+	//fill('white')
+	fill(200, mouseX%200, mouseY%200)
+	rect(695 + windows, 170 + windowss, w, l)
+	rect(755 + windows, 140 + windowss, w, l)
+	rect(587 + windows, 225 + windowss, w, l)
+	fill(700, mouseY%100, mouseX%100)
+	rect(695 + windows, 140 + windowss, w, l)
+	rect(755 + windows, 200 + windowss, w, l)
+	fill(50, mouseX%100, mouseY%200)
+	rect(695 + windows, 200 + windowss, w, l)
+	fill(500, mouseX%400, mouseY%400)
+	rect(695 + windows, 230 + windowss, w, l)
+	rect(755 + windows, 170 + windowss, w, l)
+	rect(587 + windows, 195 + windowss, w, l)
+	fill(100, mouseY%800, mouseX%800)
+	rect(587 + windows, 165 + windowss, w, l)
+	rect(755 + windows, 230 + windowss, w, l)
+  }
+  
